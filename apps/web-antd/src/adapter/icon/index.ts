35992a12568775b204bdc34b8b850ea174cece9e
zmlo1 <yujinh.333@gmail.com>
2025-02-21 20:38:18 +0800

    feat: 新增项目图标

---
+++ apps/web-antd/src/adapter/icon/index.ts
@@ -0,0 +1,8 @@
+import { createIconifyIcon } from '@vben/icons';
+
+export const MaterialSymbolsSearchRounded = createIconifyIcon(
+  'material-symbols:search-rounded',
+);
+export const MaterialSymbolsAddRounded = createIconifyIcon(
+  'material-symbols:add-rounded',
+);
